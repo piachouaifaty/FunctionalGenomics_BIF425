e040c5a0e3c85cdc618ac4514957f722263797ed
piachouaifaty <piachouaifaty@gmail.com>
2020-12-13 11:59:08 +0200

    fastq report

--- Final_NGS_Pipeline.sh
+++ Final_NGS_Pipeline.sh
@@ -16,3 +16,13 @@ zcat 392_2.fastq.gz | wc -l
 
 #Reference chromosome
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chr13.fa.gz
+
+#Running FastQC
+time fastqc -o FastQCResults 392_1.fastq.gz  392_2.fastq.gz
+
+#real    12m33.402s
+#user    12m24.276s
+#sys     0m14.858s
+
+#Copy html report files into my machine
+scp -r pia.chouaifaty@linuxdev.accbyblos.lau.edu.lb:FunctionalFinalProject/FastQCResults /Users/piachouaifaty
